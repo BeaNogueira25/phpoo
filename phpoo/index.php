d5e57c37f4a9f7bdacf17a312e29193b1154413d
BeaNogueira25 <141354064+BeaNogueira25@users.noreply.github.com>
2023-08-10 16:04:40 -0300

    commit3

--- phpoo/index.php
+++ phpoo/index.php
@@ -1,41 +1,59 @@
+
 <?php
+require ("Carro.php");
+require ("Retangulo.php");
 //nome da classe
 class Fruta{
     //nome dos atributos
-    public $nome;
+    private $nome;
     public $cor;
 
-    /*Metodos*/
-    function _construct($nome, $cor)
-    {
-       $this->nome = $nome;
-       $this->cor = $cor;
-    }
+    /*Métodos */
 
+    function __construct($nome, $cor)
+    {
+        $this->nome = $nome;
+        $this->cor = $cor;
+    }
     function set_name($nome){
         $this->nome = $nome;
     }
     function set_cor($cor){
-        $this->cor =$cor;
+        $this->cor = $cor;
     }
-    //
+    //  
     function get_name(){
         return $this->nome;
     }
     function get_cor(){
         return $this->cor;
     }
+
 }
-//a palavra new cria um objeto do tipo fruta 
-$maca = new Fruta("maca_ifsp", "Vermelho");
-/*atribuindo um nome par o meu objeto atraves do metodo  set_name*/
-$maca->set_name("maca_ifsp");
-// o  comando abaixo imprimi se a atributo for public 
+//A palavra new cria um objeto do tipo Fruta
+$maca = new Fruta("maca_ifsp","vermelho");
+/* Atribuindo um nome para o meu objeto 
+através do métodos set_name */
+//$maca->set_name("maca_ifsp");
+// o comando abaixo imprime se a atributo for public
 //echo $maca->nome;
-/*  o  comando abaixo imprimi se a atributo for public  ou private*/
+/* o comando abaixo imprime se o atributo 
+for public ou private */
 echo "</br>".$maca->get_name();
-$maca->set_cor("Vermelho");
+//$maca->set_cor("vermelho");
 echo "</br>".$maca->get_cor();
 
-?>
+$carro = new Carro("Hyndai","hb20","2015");
+echo "</br>".$carro->get_marca();
+echo "</br>".$carro->get_modelo();
+echo "</br>".$carro->get_ano();
 
+$retangulo_ifsp = new Retangulo(2,4);
+echo "</br>".$retangulo_ifsp->calcular_area();
+echo "</br>".$retangulo_ifsp->calcular_perimetro();
+
+
+
+
+
+?>
